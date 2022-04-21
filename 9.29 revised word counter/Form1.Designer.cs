e28a1c6a4d59a28888bd387249be433db0fe69c5
Jacob Neeley <jcn73@txstate.edu>
2022-04-21 10:59:57 -0500

    Push to code base

---
+++ 9.29 revised word counter/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace _9._29_revised_word_counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadTextFileButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // loadTextFileButton
+            // 
+            this.loadTextFileButton.Location = new System.Drawing.Point(12, 12);
+            this.loadTextFileButton.Name = "loadTextFileButton";
+            this.loadTextFileButton.Size = new System.Drawing.Size(129, 87);
+            this.loadTextFileButton.TabIndex = 0;
+            this.loadTextFileButton.Text = "Load Text File";
+            this.loadTextFileButton.UseVisualStyleBackColor = true;
+            this.loadTextFileButton.Click += new System.EventHandler(this.loadTextFileButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(358, 308);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(129, 87);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(147, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(205, 384);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 407);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.loadTextFileButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadTextFileButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ 9.29 revised word counter/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace _9._29_revised_word_counter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        StreamReader inputfile;
+        string textfile;
+        int frequency;
+
+        private void loadTextFileButton_Click(object sender, EventArgs e)
+        {// Read Text File.
+            listView1.Items.Clear();
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    inputfile = File.OpenText(openFileDialog.FileName);
+                    while (!inputfile.EndOfStream)
+                    {
+                        textfile = inputfile.ReadToEnd();
+                        LoadFile();
+
+                    }
+                    inputfile.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+        private void LoadFile()
+        {
+            string phrase = textfile;
+            phrase = textfile.ToLower();
+            Regex regex = new Regex("[^a-z A-Z 0-9 ' ,]");
+            phrase = regex.Replace(phrase, " ");
+            phrase = phrase.Replace(",", "");
+            String[] words = phrase.Split(' ', '.', ',', '!', '?', '\n', '\r', '\t', '\v');
+            //will seperate words such as "thats" and "that's" as 2 seperate words, otherwise wed get a few instances of "s" etc as its own word
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                frequency = 1;
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    if (words[i].Equals(words[j]))
+                    {
+                        words[j] = "0";
+                        frequency++;
+                    }
+                }
+                if (frequency > 0 && words[i] != "0" && words[i] != "")
+                {
+                    String[] rowItem = new string[] { words[i], frequency.ToString() };
+                    
+                    listView1.Items.Add(new ListViewItem(rowItem));
+                }
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Columns.Add("Word:", 120 );
+            listView1.Columns.Add("Frequency:", 100);
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.Sorting = SortOrder.Ascending;
+        }
+    }
+}
